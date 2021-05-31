56579bc48239438a4e672693d5da3651fe778913
DennisAdrianGarcia <72856216+DennisAdrianGarcia@users.noreply.github.com>
2021-05-31 16:28:08 +0800

    Update config.php

--- logApp-Garcia/logApplication/config/config.php
+++ logApp-Garcia/logApplication/config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', 'localhost/logApp-scaling-octo/');
-	define('DB_HOST', 'localhost:3301');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'admin');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6414990');
+	define('DB_PASS', 'QfdmKS7flZ');
+	define('DB_NAME', 'sql6414990');
