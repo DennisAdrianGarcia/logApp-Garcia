5fedd2ecc6541badb7f573accbbbc96c4dab8ec3
DennisAdrianGarcia <72856216+DennisAdrianGarcia@users.noreply.github.com>
2021-05-31 16:21:07 +0800

    Modified

---
+++ logApp-Garcia/logApplication/logApp-Garcia/logApplication/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'localhost/logApp-scaling-octo/');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6414990');
+	define('DB_PASS', 'QfdmKS7flZ');
+	define('DB_NAME', 'sql6414990');
